3b685aacebe2e562ba54a9e039f92b700ba3345e
Mark Martin <assplecake@gmail.com>
2016-01-28 12:10:39 -0800

    return access control for all things

--- nginx/lualib/access_router.lua
+++ nginx/lualib/access_router.lua
@@ -1,18 +1,17 @@
 local resolver = require("resolver")
 
--- firstly, authorize the user
-if ngx.var.request_method == "OPTIONS" then
-  local user = true
-  local origin = ngx.var.http_origin or ""
-  local cors, err = ngx.re.match(origin, "^https?://(\\w+\\.)?(opsy\\.co|opsee\\.com?|localhost(:\\d+)|coreys-mbp-8(:\\d+))$", "ajo")
-  if (err ~= nil or cors == nil) then
-    ngx.log(ngx.ERR, "CORS domain doesn't match: ", origin)
-    ngx.exit(ngx.HTTP_OK)
-  end
+local origin = ngx.var.http_origin or ""
+local cors, err = ngx.re.match(origin, "^https?://(\\w+\\.)?(opsy\\.co|opsee\\.com?|localhost(:\\d+)|coreys-mbp-8(:\\d+))$", "ajo")
+if (err == nil and cors ~= nil) then
   ngx.header["Access-Control-Allow-Methods"] = "GET,POST,PUT,PATCH,DELETE,HEAD"
   ngx.header["Access-Control-Max-Age"] = "1728000"
   ngx.header["Access-Control-Allow-Headers"] = "Accept-Encoding,Authorization,Content-Type"
   ngx.header["Access-Control-Allow-Origin"] = ngx.var.http_origin
+end
+
+-- firstly, authorize the user
+if ngx.var.request_method == "OPTIONS" then
+  local user = true
   ngx.exit(ngx.HTTP_OK)
 else
   local user = jose.auth_by_header()
